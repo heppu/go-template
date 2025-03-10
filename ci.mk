7a4e0d7b909b75aa7d3c004ab2616e8aab33caed
Henri Koski <henri.t.koski@gmail.com>
2025-03-10 20:10:49 +0200

    Refactor CI checks

---
+++ ci.mk
@@ -0,0 +1,48 @@
+include Makefile
+
+PUSH_TARGETS := ${TARGET_NAMES:%=push/%}
+
+.PHONY: ${PUSH_TARGETS}
+${PUSH_TARGETS}:
+	docker push --all-tags ${IMG_NAME}
+
+.PHONY: .push
+push: ${PUSH_TARGETS}
+
+.PHONY: pr-check
+pr-check: check-duplicated-migrations check-modified-migrations check-generated-code
+
+.PHONY: .main-check
+.main-check: check-duplicated-migrations check-generated-code
+
+.PHONY: check-duplicated-migrations
+.ONESHELL:
+check-duplicated-migrations:
+	@echo "Performing duplicated migration check"; \
+	output="$$(ls -1 store/migrations/ | cut -d "_" -f1 | uniq -D)"; \
+	if [ -n "$$output" ]; then \
+		echo "Found duplicate migration versions:"; \
+		echo "$$output"; \
+		exit 1; \
+	fi; \
+	echo "No duplicated migrations found"
+
+.PHONY: check-modified-migrations
+check-modified-migrations:
+	@if test -z "$$BASE_REF"; then \
+		echo "BASE_REF must be set"; \
+		exit 1; \
+	fi; \
+	if test -z "$$HEAD_REF"; then \
+		echo "HEAD_REF must be set"; \
+		exit 1; \
+	fi; \
+	echo "Performing migration verification on PR against $$BASE_REF"; \
+	git fetch origin $$BASE_REF; \
+	git fetch origin $$HEAD_REF; \
+	git diff --exit-code --name-only --diff-filter=D origin/$$BASE_REF origin/$$HEAD_REF -- store/migrations/ || (echo "main branch has new migrations, please rebase" && exit 1); \
+	echo "No modified migrations found";
+
+.PHONY: check-generated-code
+check-generated-code: generate
+	@git diff --exit-code --name-only || (printf "Running make generate modified code base.\nRun make genrate before commiting" && exit 1)
--- rename.mk
+++ rename.mk
@@ -1,19 +1,19 @@
 URL         ?= $(shell git config --get remote.origin.url)
-MODULE      ?= $(subst ssh://,,$(subst .git,,$(subst https://,,$(URL))))
-NAME        ?= $(notdir $(MODULE))
-CMD_DIR     ?= cmd/$(NAME)
+MODULE      ?= $(subst ssh://,,$(subst .git,,$(subst https://,,${URL})))
+NAME        ?= $(notdir ${MODULE})
+CMD_DIR     ?= cmd/${NAME}
 OLD_MODULE  := github.com/heppu/go-template
-OLD_CMD_DIR	:= cmd/app
+OLD_CMD_DIR	:= cmd/demo
 
 .PHONY: rename
 rename:
 	@printf "Renaming project using following configuration:\n\n"
-	@printf "URL:    $(URL)\n"
-	@printf "MODULE: $(MODULE)\n"
-	@printf "NAME:   $(NAME)\n\n"
-	@go mod edit -module $(MODULE)
+	@printf "URL:    ${URL}\n"
+	@printf "MODULE: ${MODULE}\n"
+	@printf "NAME:   ${NAME}\n\n"
+	@go mod edit -module ${MODULE}
 	@find . -type f -name '*.go' -exec sed -i 's|${OLD}|${MODULE}|g' {} \;
-	@mv cmd/app/ cmd/$(NAME)
-	@sed -i 's|${OLD_CMD_DIR}|${CMD_DIR}|g' ./applicationtest/app_test.go
+	@mv cmd/app/ cmd/${NAME}
+	@sed -i 's|${OLD_CMD_DIR}|${CMD_DIR}|g' ./applicationtest/application_test.go
 	@printf "\nProject renamed succesfully, deleting rename.mk\n"
 	@rm rename.mk
