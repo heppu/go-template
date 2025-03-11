af5ba3d797eee1b070cade3de8f9a00f0ac7c3db
heppu <henri.t.koski@gmail.com>
2025-03-11 09:19:09 +0200

    Fix migration check

--- ci.mk
+++ ci.mk
@@ -18,7 +18,8 @@ pr-check: check-duplicated-migrations check-modified-migrations check-generated-
 .PHONY: check-duplicated-migrations
 .ONESHELL:
 check-duplicated-migrations:
-	@echo "Performing duplicated migration check"; \
+	@set -e; \
+	echo "Performing duplicated migration check"; \
 	output="$$(ls -1 store/migrations/ | cut -d "_" -f1 | uniq -D)"; \
 	if [ -n "$$output" ]; then \
 		echo "Found duplicate migration versions:"; \
@@ -29,7 +30,8 @@ check-duplicated-migrations:
 
 .PHONY: check-modified-migrations
 check-modified-migrations:
-	@if test -z "$$BASE_REF"; then \
+	@set -e; \
+	if test -z "$$BASE_REF"; then \
 		echo "BASE_REF must be set"; \
 		exit 1; \
 	fi; \
@@ -40,8 +42,8 @@ check-modified-migrations:
 	echo "Performing migration verification on PR against $$BASE_REF"; \
 	git fetch origin $$BASE_REF; \
 	git fetch origin $$HEAD_REF; \
-	git diff --exit-code --name-only --diff-filter=D origin/$$BASE_REF origin/$$HEAD_REF -- store/migrations/ || (echo "main branch has new migrations, please rebase" && exit 1); \
-	echo "No modified migrations found";
+	git diff --exit-code --name-only --diff-filter=a origin/$$BASE_REF origin/$$HEAD_REF -- store/migrations/ || (echo "migrations out of sync, please rebase" && exit 1); \
+	echo "No modified migrations found"
 
 .PHONY: check-generated-code
 check-generated-code: generate
