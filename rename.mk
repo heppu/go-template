26137da9ac98013cd640124185829824bd4118f1
Juho Majasaari <juho.majasaari@iki.fi>
2025-03-21 14:08:50 +0200

    handle repo urls with git@github.com, support sed/make on macos

--- rename.mk
+++ rename.mk
@@ -3,8 +3,21 @@ OLD_NAME	:= demo
 OLD_CMD_DIR	:= cmd/demo
 NEW_URL     ?= $(shell git config --get remote.origin.url)
 NEW_MODULE  ?= $(subst ssh://,,$(subst .git,,$(subst https://,,${NEW_URL})))
+# Remove protocol prefixes one by one.
+NEW_MODULE := $(patsubst https://github.com/%,github.com/%,$(NEW_MODULE))
+NEW_MODULE := $(patsubst ssh://git@github.com:%,github.com/%,$(NEW_MODULE))
+NEW_MODULE := $(patsubst git@github.com:%,github.com/%,$(NEW_MODULE))
+# Remove the trailing ".git"
+NEW_MODULE := $(patsubst %.git,%, $(NEW_MODULE))
 NEW_NAME    ?= $(notdir ${NEW_MODULE})
 NEW_CMD_DIR	:= cmd/${NEW_NAME}
+
+GNU_SED_CHECK := $(shell sed --version 2>/dev/null | head -n1 | grep -i GNU)
+ifeq ($(GNU_SED_CHECK),)
+    SED_INPLACE_FLAG = -i ''
+else
+    SED_INPLACE_FLAG = -i
+endif
 
 .PHONY: rename
 rename:
@@ -13,10 +26,10 @@ rename:
 	@printf "MODULE: ${NEW_MODULE}\n"
 	@printf "NAME:   ${NEW_NAME}\n\n"
 	@go mod edit -module ${NEW_MODULE}
-	@find . -type f -name '*.go' -exec sed -i 's|${OLD_MODULE}|${NEW_MODULE}|g' {} \;
+	@find . -type f -name '*.go' -exec sed $(SED_INPLACE_FLAG) 's|${OLD_MODULE}|${NEW_MODULE}|g' {} \;
 	@mv ${OLD_CMD_DIR} ${NEW_CMD_DIR}
-	@sed -i 's|${OLD_CMD_DIR}|${NEW_CMD_DIR}|g' ./applicationtest/application_test.go
-	@sed -i 's|${OLD_NAME}|${NEW_NAME}|g' ./Makefile
-	@sed -i 's|${OLD_NAME}|${NEW_NAME}|g' ./Dockerfile
+	@sed $(SED_INPLACE_FLAG) 's|${OLD_CMD_DIR}|${NEW_CMD_DIR}|g' ./applicationtest/application_test.go
+	@sed $(SED_INPLACE_FLAG) 's|${OLD_NAME}|${NEW_NAME}|g' ./Makefile
+	@sed $(SED_INPLACE_FLAG) 's|${OLD_NAME}|${NEW_NAME}|g' ./Dockerfile
 	@printf "\nProject renamed succesfully, deleting rename.mk\n"
 	@rm rename.mk
